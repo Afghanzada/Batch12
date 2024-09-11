05b160fa40e5622d2981a99c262d05d5be4c1b17
hasib <hasib@10.0.0.138>
2024-09-10 18:15:39 -0700

    aaa

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -1,28 +1,5 @@
 package homework;
 
 public class GitHomework {
-	public static void main(String[] args) {
-		
-		System.out.println("BATCH 12: Git Class Lecture");
-		int age=12;
-		String name="John";
-		char gender = 'M';
-		char Gread = 'F';
-		//This is coming from github
-		
-		
-		
-	}
-	public static void add(int a,int b) {
-		System.out.println(a+b);
-		
-	}
-	public static void City(String Kabul) {
-		System.out.println(Kabul);
-	}
-	public static void greade(char A) {
-		System.out.println(A);
-	}
-	
 
 }
