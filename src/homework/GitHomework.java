e11d19f6901bd69ddd8889ee5c76f752ec0dbf14
Afghanzada <Hasibafghanzada10@gmail.com>
2024-09-10 18:53:24 -0700

    Update GitHomework.java

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -5,4 +5,11 @@ public class GitHomework {
 	public void display() {
 		System.out.println("Hasib");
 	}
+	/**
+
+		This is coming from github
+  		Java is fan
+    		Git is Easy
+      		API is not fun
+	*/
 }
