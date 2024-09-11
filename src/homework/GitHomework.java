6911c6f4d45b6b94f6317f74c8a220f40512eae6
hasib <hasib@10.0.0.138>
2024-09-10 19:06:41 -0700

    remove

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -2,14 +2,4 @@ package homework;
 
 public class GitHomework {
 
-	public void display() {
-		System.out.println("Hasib");
-	}
-	/**
-
-		This is coming from github
-  		Java is fan
-    		Git is Easy
-      		API is not fun
-	*/
 }
